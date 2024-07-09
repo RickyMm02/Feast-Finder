37f08933dbf63b72027f387065280bfdfb22d45b
RickyMm02 <grayyeonmm@gmail.com>
2024-07-09 20:33:38 +0530

    added new folder

---
+++ admin/src/pages/List/List.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import './List.css'
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const List = () => {
+
+  const url = 'http://localhost:5000'
+
+  const [list,setList] = useState([]);
+
+  const fetchList = async ()=> {
+    const response = await axios.get(`${url}/api/food/list`);
+    console.log(response.data)
+    if (response.data.success){
+      setList(response.data.data)
+    }
+    else{
+      toast.error("error")
+    }
+  }
+
+  useEffect(()=>{
+    fetchList()
+  },[])
+
+  return (
+    <div>
+      List
+    </div>
+  )
+}
+
+export default List
