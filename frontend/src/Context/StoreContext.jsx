abf7f87acd812abf886a47ceeb36f760082da72b
RickyMm02 <grayyeonmm@gmail.com>
2024-07-24 12:22:48 +0530

    integrated login page with login/logout functionalities

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { useEffect, createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null)
@@ -6,9 +6,9 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:5000"
-  const [token,setToken] = useState("")
-
+  const url = "http://localhost:5000";
+  const [token,setToken] = useState("");
+  
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
@@ -33,6 +33,12 @@ const StoreContextProvider = (props) => {
     return totalAmount;
   }
 
+  useEffect(()=>{
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
+    }
+  },[])
+
   const contextvalue = {
     food_list,
     cartItems,
@@ -44,6 +50,7 @@ const StoreContextProvider = (props) => {
     token,
     setToken
   }
+  
   return (
     <StoreContext.Provider value={contextvalue}>
       {props.children}
