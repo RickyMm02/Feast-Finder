34d1ab9a8e72129933badacc3e08f3a95a16d540
RickyMm02 <grayyeonmm@gmail.com>
2024-07-28 21:40:32 +0530

    added a new js file for user cart data

---
+++ backend/controller/cartController.js
@@ -0,0 +1,41 @@
+import userModel from "../models/userModel.js"
+
+//adding items to the user cart
+const addToCart = async (req,res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId)
+    let cartData = await userData.cartData;
+    if (!cartData[req.body.itemId]) 
+    {
+      cartData[req.body.itemId] = 1
+    }
+    else{
+      cartData[req.body.itemId] += 1
+    }
+    await userModel.findByIdAndUpdate(req.body.userId,{cartData});
+    res.json({success:true, message: "Item added to cart"});
+  } catch (error) {
+    console.log(error);
+    res.json({success:false, message: "errrror"});
+  }
+}
+
+//removing items from the user cart
+const removeFromCart = async (req,res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+  } catch (error) {
+    
+  }
+}
+
+//fetching user cart data
+const getCart = async (req,res) => {
+
+}
+
+export {
+  addToCart,
+  removeFromCart,
+  getCart 
+}
