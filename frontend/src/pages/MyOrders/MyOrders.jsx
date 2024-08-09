03900cb63421e9e3d44620433eaea0ae8a149439
RickyMm02 <grayyeonmm@gmail.com>
2024-08-09 22:34:12 +0530

    added new directory for myorders

---
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -0,0 +1,47 @@
+import React, { useContext, useState, useEffect } from 'react'
+import "./MyOrders.css"
+import { StoreContext } from '../../Context/StoreContext'
+import axios from 'axios'
+import { assets } from '../../assets/assets'
+
+const MyOrders = () => {
+
+  const {url,token} = useContext(StoreContext)
+  const [data,setData] = useState([])
+
+  const fetchOrders = async ()=>{
+    const response = await axios.post(url+"/api/order/userorders",{},{headers:{token}})
+    setData(response.data.data)
+  }
+
+  useEffect(()=>{
+    if (token) {
+      fetchOrders()
+    }
+  },[token])
+
+  return (
+    <div className='myorders'>
+      <h2>My Orders</h2>
+      <div className="container">
+        {data.map((order,index)=>{
+          return (
+            <div key={index} className='myorders-order'>
+              <img src={assets.parcel_icon} alt="" />
+              <p>{order.items.map((item,index)=>{
+                if (index === order.items.length - 1) {
+                  return item.name+" x "+item.quantity
+                }
+                else{
+                  return item.name+" x "+item.quantity+", "
+                }
+              })}</p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default MyOrders
