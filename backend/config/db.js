6f07bf191d1bd35e8257025b19fff7ef3c593842
RickyMm02 <grayyeonmm@gmail.com>
2024-09-18 22:35:21 +0530

    updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,5 @@ import mongoose from 'mongoose';
 
 
 export const connectDb = async ()=>{
-  await mongoose.connect('mongodb://localhost:27017/feastFinderDb',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(()=>console.log("Db connected"))
+  await mongoose.connect('mongodb://localhost:27017/feastFinderDb').then(()=>console.log("Db connected"))
 }
