e0dae099a7e51a224bf6bb9fe2b25cef7ba6e436
RickyMm02 <grayyeonmm@gmail.com>
2024-09-18 22:46:04 +0530

    updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 
-
 export const connectDb = async ()=>{
-  await mongoose.connect('mongodb://localhost:27017/feastFinderDb').then(()=>console.log("Db connected"))
+  await mongoose.connect('mongodb+srv://grayyeonmm:9j45UOwjlmSXYpKm@cluster0.1je6u.mongodb.net/FeastFinder').then(()=>console.log("Db connected"))
 }
